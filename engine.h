fa91a171ee77770d855ddef61ab1f81fd774ed5d
Aléxio Simões <alexio.simoes@gmail.com>
2014-03-19 21:48:36 +0000

    Update engine.h

--- engine.h
+++ engine.h
@@ -1,27 +1,28 @@
-#ifndef _NCTETRIS
-#define	_NCTETRIS
+#ifndef _PTETRIS
+#define	_PTETRIS
 typedef struct board *PtBoard;
 typedef struct piece *PtPiece;
-PtBoard createBoard (unsigned width,unsigned height, unsigned initx,unsigned inity,unsigned hidden);	//create a empty board
-PtPiece createPiece (unsigned width,unsigned height, unsigned rotx, unsigned roty, unsigned rotate);	//create a empty piece
-void destroyBoard (PtBoard *board);	//free the space alocated by board
-void destroyPiece (PtPiece *piece);	//free the space alocated by piece
+PtBoard boardCreate (unsigned width,unsigned height, unsigned initx,unsigned inity,unsigned hidden);	//create a empty board
+PtPiece pieceCreate (unsigned width,unsigned height, unsigned rotx, unsigned roty, unsigned rotate);	//create a empty piece
+void boardDestroy (PtBoard *board);	//free the space alocated by board
+void pieceDestroy (PtPiece *piece);	//free the space alocated by piece
 int moveDown (PtBoard board,PtPiece piece);	//move piece down on the board
 void moveRight (PtBoard board, PtPiece piece);	//move piece to right
 void moveLeft (PtBoard board, PtPiece piece);	//move piece to left
 PtPiece pieceRotate (PtBoard board, PtPiece piece);	//rotate piece
 int checkLines (PtBoard board, PtPiece piece);	//check and delete complete lines
-void setPiece(PtPiece piece,unsigned x, unsigned y,int value);	//set a point on a specified piece
-void setBoard(PtBoard board,unsigned x, unsigned y,int value);	//set a point in the board //for future use
-int getPiece(PtPiece piece,unsigned x, unsigned y);	//get a point in a specified piece
-int getBoard(PtBoard board,unsigned x, unsigned y);	//get a point in the board
-PtPiece copyPiece(PtPiece piece);	//returns a copy of a specified piece
-int initPiece(PtBoard board, PtPiece piece);
+void pieceSet(PtPiece piece,unsigned x, unsigned y,int value);	//set a point on a specified piece
+void boardSet(PtBoard board,unsigned x, unsigned y,int value);	//set a point in the board //for future use
+int pieceGet(PtPiece piece,unsigned x, unsigned y);	//get a point in a specified piece
+int boardGet(PtBoard board,unsigned x, unsigned y);	//get a point in the board
+PtPiece pieceCopy(PtPiece piece);	//returns a copy of a specified piece
+int pieceInit(PtBoard board, PtPiece piece);
 int pieceWidth(PtPiece piece);
 int pieceHeigth(PtPiece piece);
 int boardWidth(PtBoard board);
 int boardHeigth(PtBoard board);
 int boardHidden(PtBoard board);
+PtBoard boardClean(PtBoard *board);
 #endif
 
 
